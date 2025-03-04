cab8225d5afe16badc8dd95e03a7c0b6fe0c690e
BHKyung <gmlrudqkd3@vaner.com>
2025-03-04 17:14:58 +0900

    두마리닭다리

--- Java20250304_반복문/src/ch04/ex01/ForExam02.java
+++ Java20250304_반복문/src/ch04/ex01/ForExam02.java
@@ -8,9 +8,9 @@ public class ForExam02 {
 		
 		int sum = 0; 
 		
-		for(int i=1; i <= 100; i++) { // i=1 초기값, i <= 100 조건, i++ 증가
-			if(i % 2 == 1) {
-				sum += i;
+		for(int i=1; i <= 100; i++) { // 1~100번까지 반복
+			if(i % 2 == 1) { //홀수 인가?
+				sum += i; //홀수이면 누적
 				
 			}
 		}
